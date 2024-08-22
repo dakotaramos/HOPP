5bcabbe88013c31b8d82c63713b522703a13a6ec
Dakota Ramos <dakota.ramos@nrel.gov>
2024-08-22 13:04:35 -0600

    cambium_data.py: Updated code notes, added logic to save associated resource file names as object attribute

--- hopp/simulation/technologies/resource/cambium_data.py
+++ hopp/simulation/technologies/resource/cambium_data.py
@@ -46,7 +46,7 @@ class CambiumData(Resource):
 
     """
 
-    # Define a map between arguments and appropriate naming conventions for defining/identifying resource files
+    # Define a map between arguments and appropriate naming conventions for resource files
     filename_map = {'project_uuid':     {'82460f06-548c-4954-b2d9-b84ba92d63e2':'Cambium22',
                                          '0f92fe57-3365-428a-8fe8-0afc326b3b43':'Cambium23'},
                     'scenario':         {'High demand growth':'HighDemandGrowth',
@@ -61,8 +61,8 @@ class CambiumData(Resource):
                                          'Nations':'Nation'},
                     }
 
-    ## Define Cambium metrics and mappings needed to pull data from the Cambium API for use in LCA analysis
-    # Long-Run Marginal Emissions Rates
+    ## Define Cambium metrics and mappings needed to pull data from the Cambium API for use in LCA calculations
+    # Cambium Long-Run Marginal Emissions Rates
     lrmer_metric_cols = ['lrmer_co2_c',             # CO2 from direct combustion (kg/MWh)
                          'lrmer_ch4_c',             # CH4 from direct combustion (g/MWh)
                          'lrmer_n2o_c',             # N2O from direct combustion (g/MWh)
@@ -73,7 +73,7 @@ class CambiumData(Resource):
                          'lrmer_co2e_p',            # CO2 equivalent from precombustion (CO2, CH4, N20 with 100 year GWP - kg/MWh)
                          'lrmer_co2e'               # CO2 equivalent from combustion and precombustion (== lrmer_co2e_c + lrmer_co2e_p - kg/MWh)
                         ]
-    # Energy Generation Values
+    # Cambium Energy Generation Values
     gen_metric_cols = ['generation',                # Generation from all technologies (MWh)
                        'battery_MWh',               # Generation from Electric battery storage (MWh)
                        'biomass_MWh',               # Generation from Biopower and landfill gas (MWh)
@@ -133,11 +133,14 @@ class CambiumData(Resource):
         # Run init of Resource parent class
         super().__init__(lat,lon,year)
 
-        # Define attributes not captured by parent class init
+        # Set attributes not captured by parent class init
         self.project_uuid = project_uuid
         self.scenario = scenario
         self.location_type = location_type
         self.time_type = time_type
+
+        # Define attribute to store resource file names associated with class instance
+        self.resource_files = []
 
         # Check if path_resource is a directory, if yes define as self.path_resource attribute
         if os.path.isdir(path_resource):
@@ -150,6 +153,7 @@ class CambiumData(Resource):
         self.__dict__.update(kwargs)
 
         # Define year to start pulling cambium data from
+        # NOTE: Logic may change to accomodate concepts of technology year, installation period, analysis year, years of operation of a system for LCA analysis (may be handled in LCA code)
         if year <= 2025:
             self.cambium_year = 2025
         elif year <= 2030:
@@ -173,11 +177,11 @@ class CambiumData(Resource):
         else:
             raise ValueError("location_type argument must be either 'GEA Regions 2023' or 'Nations'")
 
-        # Define the filepath and file name for the resource file
+        # Define the filepath and filename for the resource file
         if filepath == "":
             filepath = os.path.join(self.path_resource, 
-                                    str(self.filename_map['project_uuid'][project_uuid]) + "_" +
-                                    str(self.filename_map['scenario'][scenario]) + "_" + str(time_type) + "_" +
+                                    str(self.filename_map['project_uuid'][self.project_uuid]) + "_" +
+                                    str(self.filename_map['scenario'][self.scenario]) + "_" + str(self.time_type) + "_" +
                                     str(self.location) + "_" + str(self.cambium_year) + ".csv")
         self.filename = filepath
 
@@ -186,16 +190,16 @@ class CambiumData(Resource):
 
         # Loop through years available in cambium data (2025 through 2050 in 5 year intervals)
         # If a resource file does not already exist in the directory or use_api flag == True, download the data
+        # Append / save the file name to self.resource_files
         for year_to_check in range(self.cambium_year, 2055, 5):
             self.year_to_check = year_to_check
-            self.filename = self.filename[:-8] + str(year_to_check) + self.filename[-4:]
+            self.filename = self.filename[:-8] + str(self.year_to_check) + self.filename[-4:]
             if not os.path.isfile(self.filename) or use_api:
                 self.download_resource()
+            self.resource_files.append(str(self.filename))
 
-        # TODO: verify purpose / functionality in wind_resource.py and solar_resource.py with Chris B.
-        self.format_data()
+        # self.format_data()
 
-        # TODO: verify if this info should be logged with Chris B.
         # logger.info("CambiumData: {}".format(self.filename))
 
     def lat_lon_to_gea(self):
@@ -218,7 +222,7 @@ class CambiumData(Resource):
         while n_tries < 5:
 
             try:
-                # Loop through emissions and 'generation' metrics and call API to pull data
+                # Loop through emissions and 'generation' metric and call API to pull data
                 for metric in self.lrmer_metric_cols + [self.gen_metric_cols[0]]:
                     # Define URL for emissions metrics and 'generation' metric
                     url="{base}?project_uuid={project_uuid}&scenario={scenario}&location_type={location_type}&latitude={latitude}&longitude={longitude}&year={year}&time_type={time_type}&metric_col={metric_col}".format(
@@ -242,9 +246,9 @@ class CambiumData(Resource):
                         raise RuntimeError("Maximum API request rate exceeded!")
                     else:
                         n_tries +=1
-                # Loop through generation metrics and call API to pull data
+                # Loop through technology specific generation metrics and call API to pull data
                 for metric in self.gen_metric_cols[1:]:
-                    # Define URL for generation metrics (metric_col='*_MWh', additional arg -> technology='<technology_map[metric_col]>')
+                    # Define URL for technology specific generation metrics (metric_col='*_MWh', additional arg -> technology='<technology_map[metric_col]>')
                     url="{base}?project_uuid={project_uuid}&scenario={scenario}&location_type={location_type}&latitude={latitude}&longitude={longitude}&year={year}&time_type={time_type}&metric_col=*_MWh&technology={technology}".format(
                     base=CAMBIUM_BASE_URL, project_uuid=self.project_uuid, scenario=self.scenario, location_type=self.location_type,
                     latitude=self.latitude, longitude=self.longitude, year=self.year_to_check, time_type=self.time_type, technology=self.technology_map[metric]
@@ -266,10 +270,6 @@ class CambiumData(Resource):
                         raise RuntimeError("Maximum API request rate exceeded!")
                     else:
                         n_tries +=1
-                # # Save the response_dict as a json file
-                # localfile = open(filename, mode="w+")
-                # json.dump(response_dict, localfile)
-                # localfile.close()
                 # Save the response dict as a csv file
                 localfile = open(filename, mode="w+")
                 w = csv.writer(localfile)
@@ -290,6 +290,8 @@ class CambiumData(Resource):
 
         return success
 
+    #NOTE: format_data() and data() not used in current implementation, these were originally setup for processing data from single file, LCA requires up to 5 cambium files and saving all data may be too large / memory intensive
+    #NOTE: As an alterative to loading all Cambium data into memory, current logic stores file names in self.resource_files which can be used to load data when needed for LCA calculations
     def format_data(self):
         """
         """
@@ -304,4 +306,5 @@ class CambiumData(Resource):
 
 # #Adhoc testing
 # if __name__ == '__main__':
-#     CambiumData(lat=42.55, lon=-90.69, year=2024)
+#     test = CambiumData(lat=42.55, lon=-90.69, year=2024)
+#     print(test.resource_files)
